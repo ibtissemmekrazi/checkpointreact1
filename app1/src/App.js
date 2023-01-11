ec37214af585382b323e9884689dd561554d235e
ibtissemmekrazi <ibtiss_mek@yahoo.fr>
2023-01-11 15:32:45 +0100

    first commit

---
+++ app1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbarr from './components/Navbarr';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulaire from './components/Formulaire';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbarr/>
+  
+    <Formulaire/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app1/src/components/Navbarr.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbarr() {
+  return (
+    <div className='nav'>
+
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbarr
